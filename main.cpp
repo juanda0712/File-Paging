05812d317c15264c2e04766406b620df96d76428
Juan Rodriguez <juanrm071299@gmail.com>
2021-08-24 12:22:17 -0600

    proyecto con compilador

--- main.cpp
+++ main.cpp
@@ -1,4 +1,17 @@
-//
-// Created by JuanDa on 8/24/2021.
-//
+#include <iostream>
+#include <string>
 
+using namespace std;
+
+int main()
+{
+    string palabra;
+
+    cout << "ingrese el codigo" << endl;
+
+    getline(cin,palabra);
+
+    cout << palabra << endl;
+
+    return 0;
+}
